4b1efc96e6661aa46053a27999c111f85a37eaad
longwei <1786082175@qq.com>
2021-11-24 22:45:00 +0800

    feature_添加底部导航栏、添加按钮徽标提示

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,7 +1,7 @@
 /**首页 */
 import { View, Image, Swiper, SwiperItem, Text } from "@tarojs/components";
 import "./index.scss";
-import { AtList, AtListItem, AtAvatar, AtIcon, AtGrid, AtBadge } from "taro-ui";
+import { AtAvatar, AtIcon, AtGrid } from "taro-ui";
 import { useState } from "react";
 
 import MyCard from "./components/MyCard";
@@ -13,10 +13,10 @@ const rotateImg = [
   "https://imgcps.jd.com/ling4/100014234328/5Liq5oqk54iG5qy-5aW954mp/54iG5qy-56ys5LqM5Lu25Y2K5Lu3/p-5bd8253082acdd181d02fa68/d1453fcb/cr/s/q.jpg",
 ];
 
-interface MainProps { }
+interface MainProps {}
 
 function Main(props: MainProps) {
-  const { } = props;
+  const {} = props;
   const [currentIndicator, setCurrentIndicator] = useState<number>(0);
 
   return (
@@ -108,15 +108,21 @@ function Main(props: MainProps) {
         </MyCard>
         {/**会员权益 */}
         <MyCard
-          style={{ position: 'relative', top: '-1rem' }}
+          style={{ position: "relative", top: "-1rem" }}
           upperLeft={
-            <View style={{ margin: "0.8rem 0.5rem", }}>
-              <Text style={{ marginRight: '0.2rem' }}>会员权益</Text>
+            <View style={{ margin: "0.8rem 0.5rem" }}>
+              <Text style={{ marginRight: "0.2rem" }}>会员权益</Text>
               <AtIcon value="help" size="13" color="#b2b2b2" />
             </View>
           }
           upperRight={
-            <View style={{ margin: "0.8rem 0.5rem", fontSize: '0.8rem', color: '#b2b2b2' }}>
+            <View
+              style={{
+                margin: "0.8rem 0.5rem",
+                fontSize: "0.8rem",
+                color: "#b2b2b2",
+              }}
+            >
               如何升级/降级保级
               <AtIcon value="chevron-right" size="10" />
             </View>
@@ -161,15 +167,77 @@ function Main(props: MainProps) {
                 },
               ]}
             />
-            {/* <AtGridItem></AtGridItem> */}
-            {/* </AtGrid> */}
-            {/* <AtBadge value={10} maxValue={99}>
-              <Text>我的</Text>
-            </AtBadge> */}
           </View>
         </MyCard>
-
-        <AtList>
+        {/**我的订单 */}
+        <MyCard
+          upperLeft={
+            <View style={{ margin: "0.8rem 0.5rem" }}>
+              <Text style={{ marginRight: "0.2rem" }}>我的订单</Text>
+            </View>
+          }
+        >
+          <View className="myOrders">
+            <View className="myOrderBtns">
+              <View
+                className="myOrderBtnAndDot"
+                style={{
+                  background: "#f02e72",
+                  color: "#FFF",
+                  border: "none",
+                }}
+              >
+                线上
+              </View>
+              <View className="myOrderBtnAndDot">
+                门店
+                <Text className="dot">12</Text>
+              </View>
+              <View className="myOrderBtnAndDot">自提</View>
+            </View>
+            <View className="myOrderAction">
+              <AtGrid
+                mode="rect"
+                hasBorder={false}
+                columnNum={4}
+                data={[
+                  {
+                    // iconInfo: {}
+                    image:
+                      "https://img12.360buyimg.com/jdphoto/s72x72_jfs/t6160/14/2008729947/2754/7d512a86/595c3aeeNa89ddf71.png",
+                    value: "会员抽奖",
+                  },
+                  {
+                    image:
+                      "https://img20.360buyimg.com/jdphoto/s72x72_jfs/t15151/308/1012305375/2300/536ee6ef/5a411466N040a074b.png",
+                    value: "福币兑换",
+                  },
+                  {
+                    image:
+                      "https://img10.360buyimg.com/jdphoto/s72x72_jfs/t5872/209/5240187906/2872/8fa98cd/595c3b2aN4155b931.png",
+                    value: "商家联盟",
+                  },
+                  {
+                    image:
+                      "https://img12.360buyimg.com/jdphoto/s72x72_jfs/t10660/330/203667368/1672/801735d7/59c85643N31e68303.png",
+                    value: "包邮卡",
+                  },
+                  {
+                    image:
+                      "https://img14.360buyimg.com/jdphoto/s72x72_jfs/t17251/336/1311038817/3177/72595a07/5ac44618Na1db7b09.png",
+                    value: "生日礼包",
+                  },
+                  {
+                    image:
+                      "https://img30.360buyimg.com/jdphoto/s72x72_jfs/t5770/97/5184449507/2423/294d5f95/595c3b4dNbc6bc95d.png",
+                    value: "退货补偿卷",
+                  },
+                ]}
+              />
+            </View>
+          </View>
+        </MyCard>
+        {/* <AtList>
           {new Array(20)
             .fill({
               title: "标题文字",
@@ -180,7 +248,22 @@ function Main(props: MainProps) {
             .map((item: any, index: number) => (
               <AtListItem {...item} title={`${item.title}${index}`} />
             ))}
-        </AtList>
+        </AtList> */}
+      </View>
+      {/**底部导航栏 */}
+      <View className="bottomNavigation">
+        <View className="bottomNavigationCenter">
+          <AtIcon value="money" size="30" color="#FFF"></AtIcon>
+          会员卡
+        </View>
+        <View className="bottomNavigationAction">
+          <AtIcon value="user" size="35" color="#ff4e80"></AtIcon>
+          <Text style={{ color: "#ff4e80" }}>我</Text>
+        </View>
+        <View className="bottomNavigationAction">
+          <AtIcon value="camera" size="35" color="#c0c0bf"></AtIcon>
+          生活
+        </View>
       </View>
     </View>
   );
